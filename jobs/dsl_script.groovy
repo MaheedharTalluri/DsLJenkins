6a997daa5051d595232bb8970caa0378b312939c
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-23 21:52:57 +0530

    test

--- jobs/dsl_script.groovy
+++ jobs/dsl_script.groovy
@@ -1,5 +1,19 @@
-evaluate(new File("/var/lib/jenkins/workspace/testseed/jobs/final.groovy"))
+/*evaluate(new File("/var/lib/jenkins/workspace/testseed/jobs/final.groovy"))*/
+
+
+import groovy.json.*
+
+def jS = new JsonSlurper()
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/testseed/sample.json"),"UTF-8"))
+data = jS.parse(reader)  
+String job=data.ci.jobs.job.job_name
 println(job)
+
+
+
+
+
+
 pipelineJob('job') {
 
 
