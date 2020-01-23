c860f87ee77d82abdd7ac2f7b5d766b3600b3114
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-23 14:35:53 +0530

    commited dsl files

---
+++ jobs/dsl-script.groovy
@@ -0,0 +1,21 @@
+
+
+pipelineJob('job1') {
+definition{
+cpsScm {
+scm {
+        git {
+            remote {
+                name('origin')
+                url('https://github.com/maheedhar132/Devops_assesment.git')
+            }
+            
+        }
+    }
+
+
+
+
+}
+}
+}
--- jobs/dsl.groovy
+++ jobs/dsl.groovy
@@ -10,6 +10,7 @@ script(readFileFromWorkspace('./jobs/project-workflow.groovy'))
 
 }
 
+
 }
 
 
