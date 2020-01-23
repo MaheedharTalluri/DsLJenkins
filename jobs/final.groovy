185386d22692ce57ab3a8774329f79ee870f341d
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-24 00:18:42 +0530

    test

--- jobs/final.groovy
+++ jobs/final.groovy
@@ -6,7 +6,7 @@ def data = jS.parse(reader)
 String j1=data.ci.jobs.job.job_name
 String j2=j1.replace("[","");
 String job=j2.replace("]","");
-static def jobname=job
-
+def jobname=job
+System.setProperty("job_name", "jobname")
 //context.testCase.setPropertyValue('JOB_NAME', 'jobname')
 evaluate(new File("/var/lib/jenkins/workspace/testseed/jobs/dsl_script.groovy"))
