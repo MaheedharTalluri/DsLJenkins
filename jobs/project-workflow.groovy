fba88e98fdc91c682c51c3291e439d37953c0607
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-23 14:21:18 +0530

    commited dsl files

--- jobs/project-workflow.groovy
+++ jobs/project-workflow.groovy
@@ -1,6 +1,6 @@
 
 
-/*checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[ url: 'https://github.com/maheedhar132/Devops_assesment.git']]])*/
+
 
 
 cpsScm {
