c6cf41932987ef1ceee996b3fe2bad59d0683b60
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-23 18:19:52 +0530

    json added

--- jobs/dsl_script.groovy
+++ jobs/dsl_script.groovy
@@ -1,10 +1,5 @@
-scripts{
-
-import groovy.json.*
-
-def jS = new JsonSlurper()
-var=jsonSlurper.parseText('/sample.json')
-def job=var['job_name']
+groovyScriptFile(var.groovy){
+classpath('.')
 }
 
 
