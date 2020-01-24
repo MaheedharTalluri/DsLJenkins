4cd612a35b69be7363bfbe3da442429107f73aef
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-24 14:34:40 +0530

    test

--- jobs/dsl_script.groovy
+++ jobs/dsl_script.groovy
@@ -5,7 +5,7 @@ evaluate(new File("/var/lib/jenkins/workspace/testseed/jobs/connect.groovy"))
 
 GroovyShell shell = new GroovyShell()
 def tools = shell.parse(new File("/var/lib/jenkins/workspace/testseed/jobs/connect.groovy"))
-println(connect.file)
+println(tools.connect.file)
 
 
 
