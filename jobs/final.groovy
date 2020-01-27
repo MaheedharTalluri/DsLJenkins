bdefc31fb2e073893378f411585cda8d2a2ac277
Komaravelli Manikanta (Digital) <KO20094862@wipro.com>
2020-01-27 17:18:26 +0530

    dsl edited to create hourly builds

--- jobs/final.groovy
+++ jobs/final.groovy
@@ -1,7 +1,7 @@
 import groovy.json.*
 
 def jS = new JsonSlurper()
- def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/testseed/sample.json"),"UTF-8"))
+ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${env.JOB_NAME}/sample.json"),"UTF-8"))
 def data = jS.parse(reader)  
 String j1=data.ci.jobs.job.job_name
 String j2=j1.replace("[","");
